82945abe08ccf4e6f03cd2362d76810e4138e9ac
Albert Diones <adiones@addteam.org>
2025-04-25 00:08:52 +0800

    day10 - split string using a delimiter

--- cSharp/day10.cs
+++ cSharp/day10.cs
@@ -5,11 +5,11 @@ string emailAddress = "juan.delacruz@gmail.com";
 
 string[] emailParts = emailAddress.Split('@');
 
-System.Console.WriteLine(
+Console.WriteLine(
     emailParts[0]
 );
 
-System.Console.WriteLine(
+Console.WriteLine(
     emailParts[1]
 );
 
