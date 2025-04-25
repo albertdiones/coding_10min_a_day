e4930f6722a82d0cd614e46be5807a367edbe645
Albert Diones <adiones@addteam.org>
2025-04-25 16:50:07 +0800

    day11 - c# - splitting a string into a character array

---
+++ cSharp/day11.cs
@@ -0,0 +1,30 @@
+
+using System;
+
+string name1 = "John";
+
+// ['J','o','h','n']
+char[] nameParts = name1.ToCharArray();
+
+// J
+Console.WriteLine(
+    nameParts[0]
+);
+
+// o
+Console.WriteLine(
+    nameParts[1]
+);
+
+// h
+Console.WriteLine(
+    nameParts[2]
+);
+
+// n
+Console.WriteLine(
+    nameParts[3]
+);
+
+
+
