a2bb853d5b769fe61560240b846c61e911a06a19
Albert Diones <adiones@addteam.org>
2025-04-26 20:09:03 +0800

    csharp - day12

---
+++ cSharp/day12.cs
@@ -0,0 +1,19 @@
+
+using System;
+
+string[] names = [
+    "John",
+    "Clark",
+    "Tanggol",
+    "Michael",
+    "Raull",
+    "Sahur",
+    "Abby",
+    "Marky",
+    "Gab",
+    "Raide"
+];
+
+foreach(string name in names) {
+    Console.WriteLine(name);
+}
