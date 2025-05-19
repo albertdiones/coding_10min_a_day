edad18c3529638c42828deae5bcd3128ddce8cee
Albert Diones <adiones@addteam.org>
2025-05-19 21:12:30 +0800

    day32 - print out current date; then print out current time; using 2 separate function call, but outputting on the same line

---
+++ cSharp/day32.cs
@@ -0,0 +1,13 @@
+
+using System;
+Console.Write(
+    "Current Date/Time: "
+);
+Console.Write(
+    DateTime.Now.ToString("MMM dd yyyy")
+);
+Console.Write(
+    " "+
+    DateTime.Now.ToString("HH:mm:ss K") +
+    "\n"
+);
