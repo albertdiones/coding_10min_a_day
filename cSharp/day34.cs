7bb5f8b3e36bb4f67bfa804dd4129067273f286e
Albert Diones <adiones@addteam.org>
2025-05-23 21:12:41 +0800

    day34 - print out the current date time but with the specific format: Mon Jan 13 2025 14:54:30

---
+++ cSharp/day34.cs
@@ -0,0 +1,13 @@
+using System;
+
+Console.Write(
+    "Current Date/Time: "
+);
+Console.Write(
+    DateTime.Now.ToString("ddd MMM dd yyyy")
+);
+Console.Write(
+    " "+
+    DateTime.Now.ToString("HH:mm:ss") +
+    "\n"
+);
