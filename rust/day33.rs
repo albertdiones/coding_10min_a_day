6d5aed014810dc0607dbd9b9ffe6dbc21518c87d
Albert Diones <adiones@addteam.org>
2025-05-28 21:43:20 +0800

    stash

--- rust/day33.rs
+++ rust/day33.rs
@@ -6,6 +6,18 @@ fn main() {
         Ok(duration) => {
             let timestamp: u64 = duration
                 .as_secs();
+
+            let microSecondsSubSec: u64 = duration
+                .subsec_micros();
+
+            let remainingUs: u64 = 1 - microSecondsSubSec;
+
+
+            let secondsElapsed: u64 = timestamp % 86400;
+
+
+            let remainingSeconds: u64 = 86400 - secondsElapsed;
+            
             println!("Timestamp: {:?}", timestamp);
         }
         Err(e) => {
