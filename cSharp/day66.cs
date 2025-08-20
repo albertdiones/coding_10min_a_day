1d6a54a842ccd164b9afb9162316926615d42e1b
Albert Diones <adiones@addteam.org>
2025-08-20 10:43:35 +0800

    day66 - Ask for a password input and save to a variable (and print out)

---
+++ cSharp/day66.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Text.RegularExpressions;
+
+
+
+Console.WriteLine("Input your password");
+
+ConsoleKeyInfo keyInfo;
+
+string password1 = "";
+
+while (true)
+{
+    keyInfo = Console.ReadKey(intercept: true); // intercept=true means don't display the key
+
+
+    if (keyInfo.Key == ConsoleKey.Enter)
+    {
+        break;
+    }
+    if (keyInfo.Key == ConsoleKey.Backspace) {
+        Console.Write("\b");
+        Console.Write(" ");
+        Console.Write("\b");
+        password1 = password1.Substring(0,password1.Length -1);
+        continue;
+    }
+    if (keyInfo.KeyChar > 30) {
+        password1 += keyInfo.KeyChar;
+        Console.Write('*');
+    }
+}
+Console.WriteLine("");
+Console.WriteLine("your password is: " + password1);
