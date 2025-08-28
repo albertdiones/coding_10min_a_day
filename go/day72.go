78f9e3b78ccba20c509b233500044061da73d5ba
Albert Diones <adiones@addteam.org>
2025-08-28 10:01:03 +0800

    day72 - Ask for a password and validate using a stored sha1 hash on txt file

---
+++ go/day72.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"bufio"
+	"os"
+	"golang.org/x/term"
+	"crypto/sha1"
+)
+
+func main() {
+
+	// Put terminal in raw mode (no echo, char-by-char input)
+	fd := int(os.Stdin.Fd())
+	oldState, err := term.MakeRaw(fd)
+	if err != nil {
+		panic(err)
+	}
+	// Always restore terminal on return
+	defer term.Restore(fd, oldState)
+
+
+	fmt.Println("Please input your password");
+
+	password1 := "";
+
+	for {
+		reader := bufio.NewReader(os.Stdin);
+
+		char1, _, err := reader.ReadRune();
+
+		if err != nil {
+			panic(err);
+		}
+
+		if (char1 == '\n' || char1 == '\r') {
+			break;
+		}
+
+		if (char1 == 8 || char1 == 127) {
+			fmt.Print("\b \b");
+			password1 = password1[:len(password1)-1]
+			continue;
+		}
+
+		if (char1 > 31) {
+			password1 += string(char1);
+			fmt.Print("*");
+		}
+
+	}
+
+	sha1Hasher := sha1.New();
+	sha1Hasher.Write([]byte(password1));
+	hash := fmt.Sprintf("%x", sha1Hasher.Sum(nil));
+
+	correctHash,_ := os.ReadFile("__day72.txt");
+	if (hash == string(correctHash)) {
+		fmt.Println("\nSuccessfully logged in");
+	} else {
+		fmt.Println("\nAccess denied");
+	}
+
+}
