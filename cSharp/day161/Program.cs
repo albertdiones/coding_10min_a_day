55e6d035ea695b5156a7a6ca804f2a83f11fd95f
Albert Diones <adiones@addteam.org>
2025-12-23 20:07:49 +0800

    day160 copied day161

---
+++ cSharp/day161/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+string connectionString = "Server=localhost;User ID=root;Password=543210-1;";
+
+using (MySqlConnection connection = new MySqlConnection(connectionString))
+{
+    try
+    {
+        connection.Open();
+        Console.WriteLine("Successfully connected to MySQL!");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("Error: " + ex.Message);
+    }
+}
