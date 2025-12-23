7813a02794fb603c8e270baae167d85766195eda
Albert Diones <adiones@addteam.org>
2025-12-23 20:24:57 +0800

    day161 - create script that creates a database/schema

--- cSharp/day161/Program.cs
+++ cSharp/day161/Program.cs
@@ -2,12 +2,28 @@
 using MySql.Data.MySqlClient;
 string connectionString = "Server=localhost;User ID=root;Password=543210-1;";
 
+void createSchema(MySqlConnection connection, string schemaName)
+{
+   
+    string query = "CREATE SCHEMA " + schemaName ;
+    MySqlCommand cmd = new MySqlCommand(query, connection);
+    cmd.ExecuteNonQuery();
+}
+
 using (MySqlConnection connection = new MySqlConnection(connectionString))
 {
     try
     {
         connection.Open();
-        Console.WriteLine("Successfully connected to MySQL!");
+        
+        Random rand = new Random();
+
+        int x = rand.Next(1,9999999);
+
+        string schemaName = "hello" + x;
+        createSchema(connection, schemaName);
+
+        Console.WriteLine("created db: " + schemaName);
     }
     catch (Exception ex)
     {
