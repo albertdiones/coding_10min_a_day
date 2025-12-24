1e7861626fd9e084834fa4cb5e73d034fd96ba02
Albert Diones <adiones@addteam.org>
2025-12-24 21:17:41 +0800

    day161 copied to day162

---
+++ cSharp/day162/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+string connectionString = "Server=localhost;User ID=root;Password=543210-1;";
+
+void createSchema(MySqlConnection connection, string schemaName)
+{
+   
+    string query = "CREATE SCHEMA " + schemaName ;
+    MySqlCommand cmd = new MySqlCommand(query, connection);
+    cmd.ExecuteNonQuery();
+}
+
+using (MySqlConnection connection = new MySqlConnection(connectionString))
+{
+    try
+    {
+        connection.Open();
+        
+        Random rand = new Random();
+
+        int x = rand.Next(1,9999999);
+
+        string schemaName = "hello" + x;
+        createSchema(connection, schemaName);
+
+        Console.WriteLine("created db: " + schemaName);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("Error: " + ex.Message);
+    }
+}
