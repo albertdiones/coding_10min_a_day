c726004b7a263e4803674372f6a276ee7c5990b7
Albert Diones <adiones@addteam.org>
2025-12-31 15:31:17 +0800

    day166 copied to day167

---
+++ cSharp/day167/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+using System.Data.SqlClient;
+
+
+string connectionString = "Server=localhost;User ID=root;Password=543210-1;Database=hello1";
+
+
+void createSchema(MySqlConnection connection, string schemaName)
+{
+   
+    string query = "CREATE SCHEMA " + schemaName ;
+    MySqlCommand cmd = new MySqlCommand(query, connection);
+    cmd.ExecuteNonQuery();
+}
+void CreateTable(
+    MySqlConnection connection,
+    string tableName,
+    Dictionary<string, string> columns
+)
+{
+    if (columns == null || columns.Count == 0)
+        throw new ArgumentException("At least one column must be specified.");
+
+    var columnDefinitions = string.Join(", ",
+        columns.Select(c => $"{c.Key} {c.Value}")
+    );
+
+    string query = $"CREATE TABLE `{tableName}` ({columnDefinitions});";
+
+    using var cmd = new MySqlCommand(query, connection);
+    cmd.ExecuteNonQuery();
+}
+
+
+
+void InsertRow(
+    MySqlConnection connection,
+    string tableName,
+    Dictionary<string, string> row
+)
+{
+    if (row == null || row.Count == 0)
+        throw new ArgumentException("At least one column must be specified.");
+
+    var columNames = string.Join(", ",
+        row.Select(c => $"{c.Key}")
+    );
+
+    
+    var values = "'" + string.Join("', '",
+        row.Select(c => $"{c.Value}")
+    ) + "'";
+
+
+    string query = $"INSERT INTO {tableName}({columNames}) VALUES({values});";
+
+    using var cmd = new MySqlCommand(query, connection);
+    cmd.ExecuteNonQuery();
+}
+
+
+
+string SelectCount(
+    MySqlConnection connection,
+    string tableName
+)
+{
+
+    string query = $"SELECT COUNT(0) FROM {tableName}";
+
+    using var cmd = new MySqlCommand(query, connection);
+    return cmd.ExecuteScalar().ToString();
+}
+
+
+MySqlDataReader SelectRows(
+    MySqlConnection connection,
+    string tableName
+)
+{
+
+    string query = $"SELECT * FROM {tableName}";
+
+    using var cmd = new MySqlCommand(query, connection);
+
+    return cmd.ExecuteReader();
+}
+
+
+
+using (MySqlConnection connection = new MySqlConnection(connectionString))
+{
+        connection.Open();
+        
+        Random rand = new Random();
+
+        int x = rand.Next(1,9999999);
+
+        var row = new Dictionary<string, string>
+            {
+                { "id", x.ToString() },
+                {  "name", "Jay" }
+            };
+
+        string tableName = "lamesa_2861101";
+
+        InsertRow(connection, tableName, row);
+
+        Console.WriteLine("inserted row!! id: " + x);
+        Console.WriteLine("table row count: " + SelectCount(connection, tableName));
+
+
+        MySqlDataReader reader = SelectRows(connection, tableName);
+        while (reader.Read())
+        {
+            List<string> values = new List<string>();
+            for (int i = 0; i < reader.FieldCount; i++)
+            {
+                values.Add(reader.IsDBNull(i) ? "" : reader.GetValue(i).ToString());
+            }
+
+            Console.WriteLine(string.Join(',', values.ToArray()));
+        }
+}
